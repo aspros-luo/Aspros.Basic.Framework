80b6c2116c19c8f79f0dd442e17e15297a3f6239
Aspros <Aspros_L@outlook.com>
2025-01-19 15:43:40 +0800

    Add SnowFlake

---
+++ Aspros.Base.Framework.Infrastructure/SnowFlake/DisposableAction.cs
@@ -0,0 +1,9 @@
+﻿namespace Aspros.Base.Framework.Infrastructure
+{
+    public class DisposableAction(Action action) : IDisposable
+    {
+        readonly Action _action = action ?? throw new ArgumentNullException("action");
+
+        public void Dispose() => _action();
+    }
+}
